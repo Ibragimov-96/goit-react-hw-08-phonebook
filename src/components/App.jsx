9c25964db190b31673c6f413691cca82cf0a0a63
Ibragimov-96 <dima.ibragimov.1996@ukr.net>
2023-01-23 17:31:28 +0200

    03
    333

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,8 +15,10 @@ export class App extends Component {
   componentDidMount(){
     const contacts = localStorage.getItem('contacts')
     const parsedContact = JSON.parse(contacts)
+    if(parsedContact){
+      this.setState({contacts:parsedContact})
+    }
     
-    this.setState({contacts:parsedContact})
   }
 componentDidUpdate(prevProps, prevState){
   if(this.state.contacts !== prevState.contacts){
